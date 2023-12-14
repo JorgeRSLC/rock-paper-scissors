44d9d991afd9777ff34f13527c9ede68458c467c
Jorge <102831018+JorgeRSLC@users.noreply.github.com>
2023-12-14 08:03:34 -0700

    Implement UI for rock paper scissor
    Add buttons
    Add results panel
    Add images

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ let getComputerChoice = ()=>{
     }
 }
 
-let getPlayerChoice = () => prompt(`"Make your selection of "rock", "paper" or "scissor".`)
+// let getPlayerChoice = () => prompt(`"Make your selection of "rock", "paper" or "scissor".`)
 
 let calculateWinner = (playerSelection, computerSelection) => {
     if(playerSelection===computerSelection){
@@ -19,19 +19,94 @@ let calculateWinner = (playerSelection, computerSelection) => {
     }
     else if( playerSelection==="rock" && computerSelection=="scissor" ||
     playerSelection==="paper" && computerSelection=="rock" ||
-    playerSelection==="sciccor" && computerSelection=="paper"){
+    playerSelection==="scissor" && computerSelection=="paper"){
         return `You win! ${playerSelection} beats ${computerSelection}!`
     }
     else return `You loose! ${computerSelection} beats ${playerSelection}!`
 }
 
-let playRound = () => {
-    let playerSelection = getPlayerChoice();
-    let computerSelection = getComputerChoice();
-    let winnerStatement = calculateWinner(playerSelection,computerSelection);
-    if(winnerStatement==="Tie! Play again!"){
-        console.log(winnerStatement);
-        playRound();
-    } 
-    else console.log(winnerStatement);
+// let playRound = () => {
+//     let playerSelection = getPlayerChoice();
+//     let computerSelection = getComputerChoice();
+//     let winnerStatement = calculateWinner(playerSelection,computerSelection);
+//     if(winnerStatement==="Tie! Play again!"){
+//         console.log(winnerStatement);
+//         playRound();
+//     } 
+//     else console.log(winnerStatement);
+// }
+
+let playersChoice = '';
+let computerChoice = '';
+let prefix = './images/';
+let suffix = '.png';
+
+const paper = document.querySelector('.paper');
+
+paper.addEventListener('click', ()=> {
+    playersChoice = 'paper';
+    loadResults();
+})
+
+
+const rock = document.querySelector('.rock');
+
+rock.addEventListener('click', ()=> {
+    playersChoice = 'rock';
+    loadResults();
+})
+
+const scissor = document.querySelector('.scissor');
+
+scissor.addEventListener('click', ()=> {
+    playersChoice = 'scissor';
+    loadResults();
+})
+
+let loadResults = () => {
+    computerChoice = getComputerChoice()
+    setImage(prefix+playersChoice+suffix,'.left');
+    setImage(prefix+computerChoice+suffix,'.right')
+
+    let roundWinner = document.querySelector('.round-winner')
+    roundWinner.replaceChildren();
+    let winnerStatement = calculateWinner(playersChoice,
+        computerChoice);
+
+    winnerStatement = document.createTextNode(winnerStatement);
+    let tempParagraph = document.createElement('p');
+    tempParagraph.appendChild( winnerStatement);
+    roundWinner.appendChild(tempParagraph);
+
+    let tempParagraph2 = document.createElement('p');
+    let playAgain = document.createTextNode(`Press "RESET" to play next round`)
+    tempParagraph2.appendChild(playAgain);
+    roundWinner.appendChild(tempParagraph2);
+    
+    computerChoice = '';
+    playersChoice = '';
 }
+
+let setImage = (imageURL,resultPane) =>{
+    let results = document.querySelector(resultPane)
+    results.replaceChildren();
+    let image = document.createElement('img');
+    image.src = imageURL;
+    results.appendChild(image);
+}
+
+let reset = () =>{
+    computerChoice = '';
+    playersChoice = '';
+    setImage('./images/go3.png','.left');
+    setImage('./images/go3.png','.right')
+    let roundWinner = document.querySelector('.round-winner')
+    roundWinner.replaceChildren();
+    winnerStatement = document.createTextNode('Make your selection!');
+    let tempParagraph = document.createElement('p');
+    tempParagraph.appendChild( winnerStatement);
+    roundWinner.appendChild(tempParagraph);
+}
+
+const resetTiles = document.querySelector('.reset-button')
+resetTiles.addEventListener('click',reset)
